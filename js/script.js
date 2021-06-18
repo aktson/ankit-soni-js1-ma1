26bc57620b9a1efb9bef47937010aef5f562adef
Ankit Soni <akt_soni@yahoo.co.in>
2021-06-18 22:56:22 +0200

    Update script.js
    answers for all questions added for in js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+
+//question 1
+const cat = {
+    complain: function() {
+    console.log ("Meowl")
+    }
+
+};
+cat.complain();
+
+
+//question 2
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+
+//question 3
+heading.style.fontSize = "2em";
+
+
+//question 4
+heading.classList.add("subheading");
+
+
+//question 5
+const paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++) {
+
+paragraphs[i].style.color = "red" 
+
+}
+
+
+//question 6 
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML += `<p>New paragraph</p>`;
+
+
+//question 7
 const cats = [
     {
         name: "Blob",
@@ -11,3 +53,42 @@ const cats = [
         age: 21
     }
 ];
+
+function allCats (list) {
+    for (let i = 0; i < list.length; i++) {
+        console.log (list[i].name);
+    }
+};
+allCats(cats);
+
+
+//question 8
+
+function createCats (cats) {
+   
+    let html;
+
+    for (let i = 0; i < cats.length; i++) {
+
+    const ageUnKnown = "Age unknown";
+
+    if (!cats[i].age) {
+    cats[i].age = ageUnKnown; 
+    }
+
+    html += `<div>
+            <h5>Name: ${cats[i].name}</h5>
+            <p>Age: ${cats[i].age}</p>
+            </div>`;
+    }
+
+return html;
+}
+
+const catContainer = document.querySelector(".cat-container");
+
+const createdCats = createCats (cats);
+
+catContainer.innerHTML +=  createdCats;
+
+
